50eae5db6e8dfda0b29e77fd4c7fb850529cb597
U-ISART\m.mehalin <m.mehalin@Zotac-GP-24.isart.lan>
2020-09-18 16:39:06 +0200

    prototypes

---
+++ geometric_prim.h
@@ -0,0 +1,37 @@
+#pragma once
+
+typedef struct vector2
+{
+    float x;
+    float y;
+} vector2;
+
+typedef struct line
+{
+
+} line;
+
+typedef struct segment
+{
+
+} segment;
+
+typedef struct circle
+{
+
+} circle;
+
+typedef struct rect
+{
+
+} rect;
+
+typedef struct orientedRect
+{
+
+} orientedRect;
+
+typedef struct convexPolygon
+{
+
+} convexPolygon;
---
+++ vector2.c
@@ -0,0 +1,63 @@
+#include <math.h>
+
+#include "geometric_prim.h"
+
+vector2 zeroVector()
+{
+
+}
+
+vector2 unitVector()
+{
+    //verif vect nul
+}
+
+bool compareVector()
+{
+
+}
+
+float vectorMagnitude() // vector's "norme"
+{
+
+}
+
+float squareMagnitude()
+{
+
+}
+
+float distance() // between 2 pts
+{
+
+}
+
+vector2 addition()
+{
+
+}
+
+vector2 subtraction()
+{
+
+}
+
+vector2 scale() // multiplication vector/scalar
+{
+
+}
+
+vector2 negate() // opposite vector
+{
+
+}
+
+float dotProduct()
+{
+
+}
+
+float angle()
+{
+
+}
